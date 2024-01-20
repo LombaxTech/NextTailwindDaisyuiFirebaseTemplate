d03be93d9e9b7431c1db0fc26b075e9a54116337
Rakib <rakib@think-engineer.com>
2024-01-20 00:41:55 +0000

    second commit ?

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,23 @@
 import { addDoc, collection } from "firebase/firestore";
-import React from "react";
+import React, { useContext, useEffect } from "react";
 
-import { app, db } from "../firebase";
+import { app, auth, db } from "@/firebase";
+import { AuthContext } from "@/context/AuthContext";
+import { signOut } from "firebase/auth";
+import { useRouter } from "next/router";
 
 export default function App() {
+  const { user, userLoading } = useContext(AuthContext);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log("starting logs...");
+    console.log(user);
+
+    // if (!userLoading && !user) router.push("signup");
+  }, [user, userLoading]);
+
   const addStuff = async () => {
     const docRef = await addDoc(collection(db, "users"), {
       first: "Ada",
@@ -13,34 +27,23 @@ export default function App() {
     console.log("Document written with ID: ", docRef.id);
   };
 
-  return (
-    <div>
-      <div className="navbar bg-base-200">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
-        </div>
-        <div className="flex-none">
-          <button className="btn btn-square btn-ghost">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block w-5 h-5 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-              ></path>
-            </svg>
-          </button>
-        </div>
-      </div>
+  const show = async () => {
+    console.log(user);
+  };
 
-      <button onClick={addStuff} className="btn btn-primary">
+  return (
+    <div className="">
+      {/* <button onClick={addStuff} className="btn btn-primary">
         Click me!
-      </button>
+      </button> */}
+      {user && (
+        <div className="p-10">
+          {user.email}
+          {user.name}
+          <button onClick={show}>show</button>
+        </div>
+      )}
+      {!user && <div>No user found</div>}
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,9 @@ const config: Config = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    darkTheme: false,
+    darkTheme: "light",
     themes: ["light", "dark", "cupcake"],
   },
+  darkMode: "class",
 };
 export default config;
