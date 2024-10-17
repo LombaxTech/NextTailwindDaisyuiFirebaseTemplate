f85e8e190ccb057202eb555a7b7d5dd23358a499
Rakib <42938731+LombaxTech@users.noreply.github.com>
2024-10-17 23:29:58 +0100

    Auto redirect to auth if not signed in

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,24 @@
 import { AuthContext } from "@/context/AuthContext";
 import React, { useContext } from "react";
 import Navbar from "./Navbar";
+import { useRouter } from "next/router";
 
 export default function Layout({ children }: { children: any }) {
   const { user, userLoading } = useContext(AuthContext);
 
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!userLoading && !user && router.isReady) return;
+    
+    let { pathname } = router;
+    if (pathname !== "/signin" && pathname !== "/signup") {
+      router.push("/signin");
+    }
+  
+  }, [user, userLoading, router.isReady, router.pathname]);
+  
+  
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
