31522cc2e6904bb15adca4f55395f064b8599b92
Rakib <rakibkhan@live.co.uk>
2024-10-17 17:07:39 +0100

    changes to template with sign up & sign in

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,12 @@
 import { addDoc, collection } from "firebase/firestore";
-import React, { useContext, useEffect } from "react";
+import { useContext } from "react";
 
-import { app, auth, db } from "@/firebase";
-import { AuthContext } from "@/context/AuthContext";
-import { signOut } from "firebase/auth";
-import { useRouter } from "next/router";
 import SetupAccount from "@/components/SetupAccount";
+import { AuthContext } from "@/context/AuthContext";
+import { db } from "@/firebase";
 
 export default function App() {
   const { user, userLoading } = useContext(AuthContext);
-
-  const router = useRouter();
-
-  useEffect(() => {
-    console.log("starting logs...");
-    console.log(user);
-
-    // if (!userLoading && !user) router.push("signup");
-  }, [user, userLoading]);
 
   const addStuff = async () => {
     const docRef = await addDoc(collection(db, "users"), {
@@ -28,25 +17,13 @@ export default function App() {
     console.log("Document written with ID: ", docRef.id);
   };
 
-  const show = async () => {
-    console.log(user);
-  };
-
   if (!userLoading && user?.setup == false) return <SetupAccount />;
 
   return (
     <div className="">
-      {/* <button onClick={addStuff} className="btn btn-primary">
+      <button onClick={addStuff} className="btn btn-primary">
         Click me!
-      </button> */}
-      {user && (
-        <div className="p-10">
-          {user.email}
-          {user.name}
-          <button onClick={show}>show</button>
-        </div>
-      )}
-      {!user && <div>No user found</div>}
+      </button>
     </div>
   );
 }
--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,16 +1,12 @@
-import Link from "next/link";
-import { useContext, useState } from "react";
+import { auth } from "@/firebase";
 import {
-  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
   signInWithEmailAndPassword,
   signInWithPopup,
-  GoogleAuthProvider,
 } from "firebase/auth";
-import { auth, db } from "@/firebase";
-import { doc, setDoc } from "firebase/firestore";
+import Link from "next/link";
 import { useRouter } from "next/router";
-import { AuthContext } from "@/context/AuthContext";
-import GoogleButton from "@/components/GoogleButton";
+import { useState } from "react";
 
 const provider = new GoogleAuthProvider();
 
@@ -29,6 +25,8 @@ export default function SignIn() {
       console.log("res of signin in ");
       console.log(authUser);
 
+      router.push("/");
+
       setSuccess(true);
     } catch (error) {
       console.log(error);
@@ -38,113 +36,55 @@ export default function SignIn() {
 
   const signinWithGoogle = async () => {
     try {
-      signInWithPopup(auth, provider)
-        .then((result) => {
-          const user = result.user;
-          console.log(result);
-
-          router.push("/");
-        })
-        .catch((error) => {
-          console.log(error);
-          // Handle Errors here.
-          const errorCode = error.code;
-          const errorMessage = error.message;
-          // The email of the user's account used.
-          const email = error.customData.email;
-          // The AuthCredential type that was used.
-          const credential = GoogleAuthProvider.credentialFromError(error);
-          // ...
-        });
+      let result = await signInWithPopup(auth, provider);
+      // result.user
+      router.push("/");
     } catch (error) {
       console.log(error);
     }
   };
 
   return (
-    <section className="bg-gray-50 dark:bg-gray-900 flex flex-col items-center justify-center min-h-screen">
-      <div className="p-6  space-y-6 w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700">
-        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-          Sign In
-        </h1>
-        <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
-          <div>
-            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-              Your email
-            </label>
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="name@company.com"
-              required
-            />
-          </div>
-          <div>
-            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-              Password
-            </label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="••••••••"
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              required
-            />
-          </div>
-          {/* <div className="flex items-center justify-between">
-                <div className="flex items-start">
-                  <div className="flex items-center h-5">
-                    <input
-                      id="remember"
-                      aria-describedby="remember"
-                      type="checkbox"
-                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                      required
-                    />
-                  </div>
-                  <div className="ml-3 text-sm">
-                    <label
-                      htmlFor="remember"
-                      className="text-gray-500 dark:text-gray-300"
-                    >
-                      Remember me
-                    </label>
-                  </div>
-                </div>
-                <a
-                  href="#"
-                  className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
-                >
-                  Forgot password?
-                </a>
-              </div> */}
-          <button
-            type="submit"
-            className="btn btn-primary w-full"
-            // className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-          >
-            Sign In
-          </button>
-
-          {success && <p className="text-green-600">Sign-in successful!</p>}
-          {error && <p className="text-red-600">{error}</p>}
-          <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-            Don't have an account?{" "}
-            <Link
-              href={"/signup"}
-              className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-            >
-              Create an account
-            </Link>
-          </p>
-        </form>
-        <div className="">
-          <GoogleButton onClick={signinWithGoogle} />
-        </div>
+    <div className="flex-1 flex flex-col items-center gap-4 pt-20">
+      <div className="flex flex-col gap-2">
+        <h1 className="text-2xl font-bold text-center">Sign in</h1>
+        <Link href={"/signup"} className="underline text-center">
+          Don't have an account?
+        </Link>
       </div>
-    </section>
+
+      <div className="p-10 w-full lg:w-4/12 rounded-md bg-white shadow-md flex flex-col gap-6">
+        <div className="flex flex-col gap-2">
+          <label className="">Email </label>
+          <input
+            type="email"
+            className="p-2 outline-none border"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter your email"
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <label className="">Password </label>
+          <input
+            type="password"
+            className="p-2 outline-none border"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+        </div>
+        <button className="btn btn-primary" onClick={handleSubmit}>
+          Sign In
+        </button>
+        {/* <GoogleButton
+          onClick={signinWithGoogle}
+          // buttonText="Make account with Google"
+        /> */}
+        {error && (
+          <div className="p-2 bg-red-200 text-red-700 text-center">{error}</div>
+        )}
+      </div>
+    </div>
   );
 }
